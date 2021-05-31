b8240902ca7f624f51adb07e18657a9841bb1ca3
amountainram <amountainram@gmail.com>
2021-05-31 19:30:49 +0200

    edge support

--- frontend/src/utils/browser.utils.js
+++ frontend/src/utils/browser.utils.js
@@ -1,18 +1,20 @@
 export function getBrowser() {
   if (
     (navigator.userAgent.indexOf("Opera") ||
-      navigator.userAgent.indexOf("OPR")) != -1
+      navigator.userAgent.indexOf("OPR")) !== -1
   ) {
     return "Opera";
-  } else if (navigator.userAgent.indexOf("Chrome") != -1) {
+  } else if (navigator.userAgent.indexOf("Chrome") !== -1) {
     return "Chrome";
-  } else if (navigator.userAgent.indexOf("Safari") != -1) {
+  } else if (navigator.userAgent.indexOf("Safari") !== -1) {
     return "Safari";
-  } else if (navigator.userAgent.indexOf("Firefox") != -1) {
+  } else if (navigator.userAgent.indexOf("Edge") !== -1) {
+    return "Edge";
+  } else if (navigator.userAgent.indexOf("Firefox") !== -1) {
     return "Firefox";
   } else if (
-    navigator.userAgent.indexOf("MSIE") != -1 ||
-    !!document.documentMode == true
+    navigator.userAgent.indexOf("MSIE") !== -1 ||
+    !!document.documentMode === true
   ) {
     //IF IE > 10
     return "IE";
