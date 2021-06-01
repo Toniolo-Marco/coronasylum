9240d9a55c7ca5e40d4c521a77b0d40b3352bec5
marco-toniolo <marcotoniolomail@gmail.com>
2021-06-01 08:51:32 +0200

    structured page about

--- frontend/src/components/About.components.jsx
+++ frontend/src/components/About.components.jsx
@@ -36,15 +36,83 @@ export default function About() {
               <Api />
             </Tab>
             <Tab eventKey="Database" title="Database">
-              <Api />
+              <Database />
             </Tab>
             <Tab eventKey="Developer" title="Developer">
-              <Api />
+              <Developer />
             </Tab>
             <Tab eventKey="Documentation" title="Documentation">
-              <Api />
+              <Documentation />
             </Tab>
           </Tabs>
+          <Row style={{ marginTop: "10%", marginBottom: "5%" }}>
+            <Col xs={4}>
+              <Card
+                style={{ width: "18rem" }}
+                className={`${ColorStyle.bgGrey4}`}
+              >
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Non-Relational Database</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button
+                    variant="primary"
+                    target="_blank "
+                    href="https://www.mongodb.com/non-relational-database#:~:text=Non%2Drelational%20databases%20(often%20called,on%20data%20structures%20like%20documents."
+                  >
+                    Learn More
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={4}>
+              <Card
+                style={{ width: "18rem" }}
+                className={`${ColorStyle.bgGrey4}`}
+              >
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>What's an API?</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button
+                    variant="primary"
+                    target="_blank "
+                    href="https://www.redhat.com/en/topics/api/what-are-application-programming-interfaces"
+                  >
+                    Learn More
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={4}>
+              <Card
+                style={{ width: "18rem" }}
+                className={`${ColorStyle.bgGrey4}`}
+              >
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Database Management System</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button
+                    variant="primary"
+                    target="_blank "
+                    href="https://www.mongodb.com/database-management-system"
+                  >
+                    Learn More
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </Container>
       </div>
     </React.Fragment>
@@ -53,24 +121,29 @@ export default function About() {
 
 function Api() {
   return (
-    <div>
-      <Card style={{ width: "18rem" }} className={`${ColorStyle.bgGrey4}`}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>What's an API?</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button
-            variant="primary"
-            target="_blank "
-            href="https://www.redhat.com/en/topics/api/what-are-application-programming-interfaces"
-          >
-            Learn More
-          </Button>
-        </Card.Body>
-      </Card>
+    <div style={{ marginTop: "5%" }}>
+      <Row>
+        <Col>
+          <h2 className={`${ColorStyle.colorWhite1}`}>Our API:</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p className={` ${ColorStyle.colorGrey5}`}>TESTO API BLA BLA BLA</p>
+        </Col>
+      </Row>
     </div>
   );
+}
+
+function Database() {
+  return <div style={{ marginTop: "10%" }}></div>;
+}
+
+function Developer() {
+  return <div style={{ marginTop: "10%" }}></div>;
+}
+
+function Documentation() {
+  return <div style={{ marginTop: "10%" }}></div>;
 }
