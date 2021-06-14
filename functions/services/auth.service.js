514c1d8a565bc514a136a0232de389e47150fc79
amountainram <amountainram@gmail.com>
2021-06-14 14:56:15 +0200

    either defined or used or not defined at all

--- functions/services/auth.service.js
+++ functions/services/auth.service.js
@@ -1,12 +1,12 @@
 const CLIENT_ID = process.env.CLIENT_ID;
 const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client(process.env.CLIENT_ID);
+const client = new OAuth2Client(CLIENT_ID);
 
 exports.auth = async function (token) {
   return client
     .verifyIdToken({
       idToken: token,
-      audience: process.env.CLIENT_ID,
+      audience: CLIENT_ID,
     })
     .then((user) => {
       return user;
